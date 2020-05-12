773338ac2ba224be96ccfa84b63c1c385e6e3040
unknown <gibiee@naver.com>
2020-05-13 02:30:08 +0900

    .

--- human_play.py
+++ human_play.py
@@ -36,7 +36,7 @@ def run():
     # model_file = './model/policy_5000.model'
     width, height = 9, 9
     # model_file = './model/policy_9_9_10000.model'
-    model_file = '.omok_AI/model/policy_9_9_10000.model'
+    model_file = './omok_AI/model/policy_9_9_10000.model'
     
     board = Board(width=width, height=height, n_in_row=n)
     game = Game(board)
