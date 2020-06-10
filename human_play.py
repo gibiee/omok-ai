d6f6a90c9dd5a188a4f7efe76e433decfa2da1e0
unknown <gibiee@naver.com>
2020-06-10 21:01:39 +0900

    .

--- human_play.py
+++ human_play.py
@@ -12,13 +12,14 @@ class Human(object):
 
     def get_action(self, board):
         try:
-            location = input("돌을 둘 좌표를 입력 : ")
+            print("돌을 둘 좌표를 입력하세요.")
+            location = input()
             if isinstance(location, str) : location = [int(n, 10) for n in location.split(",")]
             move = board.location_to_move(location)
         except Exception as e : move = -1
             
         if move == -1 or move in board.states.keys() :
-            print("다시 입력하십시오.")
+            print("다시 입력하세요.")
             move = self.get_action(board)
         elif board.is_you_black() and tuple(location) in board.forbidden_locations :
             print("금수 자리에 돌을 놓을 수 없습니다.")
@@ -36,7 +37,7 @@ def run():
     print("이 오목 인공지능은 9x9 환경에서 동작합니다.")
     
     print("현재 가능한 난이도 목록 : []")
-    print(""난이도를 입력하세요.")
+    print("난이도를 입력하세요.")
     hard = int(input())
     model_file = f'./omok_AI/model/policy_9_{hard}.model'    # colab
     # model_file = f'./model/policy_9_{hard}.model'          # local
